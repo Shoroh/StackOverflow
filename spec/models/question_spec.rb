fb6de93764749a366d203bb4be3aae4cc08facc6
Shelestov Aleksandr <shoroh362@gmail.com>
2014-04-27 15:41:18 +0700

    Test of length is green

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,10 +4,5 @@ require 'shoulda/matchers'
 describe Question do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
-
-  # Почему следующие две строчки не работают?
-  question = FactoryGirl.build :question # — так указано в доках к FactoryGirls, но работает только вместе с "question = FactoryGirl.build :question"
-  it { should ensure_length_of(question.title).is_at_least(5).is_at_most(80) } # — всегда провальный тест.
-
-
+  it { should ensure_length_of(:title).is_at_least(5).is_at_most(80) }
 end
