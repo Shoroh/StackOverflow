adadf1a1fc8a66b760f5b3f7d2f3684bb1f7106d
Shelestov Aleksandr <shoroh362@gmail.com>
2014-05-03 10:35:13 +0700

    Seeds db, profile link, green

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'factory_girl'
+
+User.all.each do |u|
+  unless u.name == 'Shoroh'
+    u.destroy!
+  end
+end
+
+FactoryGirl.create_list(:question, 50)
+FactoryGirl.create_list(:pending_question, 20)
+FactoryGirl.create_list(:deleted_question, 20)
