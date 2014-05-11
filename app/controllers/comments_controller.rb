ca1c5064c13defbb913d756ac027db471cea38be
Shelestov Aleksandr <shoroh362@gmail.com>
2014-05-11 23:31:27 +0700

    New redirect in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     if @comment.save!
       respond_to do |format|
-        format.html {redirect_to @question}
+        format.html {redirect_to @commentable.try(:question) || @commentable}
         format.js
       end
     end
