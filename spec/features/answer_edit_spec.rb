13ec78f2927e790356a082da46725d990e062829
Shelestov Aleksandr <shoroh362@gmail.com>
2014-05-13 14:48:33 +0700

    Answer edit invalid data test green

--- spec/features/answer_edit_spec.rb
+++ spec/features/answer_edit_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Answer Create, Edit and Delete' do
+feature 'Answer Edit' do
 
   given(:user) {create(:user)}
   given(:question) {create(:question, user: user)}
@@ -22,5 +22,23 @@ feature 'Answer Create, Edit and Delete' do
     within '.answers' do
       expect(page).to have_content "New title for old question."
     end
+  end
+
+  scenario 'Registered User edits his answer with invalid data', js: true do
+    answer = create(:answer, question: question, user: user)
+    sign_in_form(user.email, user.password)
+    visit question_path(question)
+
+    within "#item-answer-#{answer.id}" do
+      click_on 'Edit'
+    end
+
+    within "#item-answer-#{answer.id}" do
+      fill_in 'Your answer', with: "2"
+      click_on 'Update Answer'
+    end
+
+    expect(page).to have_content "is too short (minimum is 3 characters)"
+
   end
 end
